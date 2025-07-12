e9a28a1c50d2836f0cb714ca4f6768854630471e
Ariel Nunes da Silva <arielnds274@gmail.com>
2025-07-12 10:46:52 -0300

    fix: mongoose connection

--- api/src/config/db_config.ts
+++ api/src/config/db_config.ts
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
+import "dotenv/config";
 
 export async function connectDB(): Promise<void> {
     try {
-        await mongoose.connect("mongodb://localhost:27017/love-link");
+        await mongoose.connect(process.env.MONGODB_URI || "mongodb://0.0.0.0:27017/love-link");
         console.log("Mongo DB connected successfully");
     } catch (err) {
         console.error("Mongo DB connection error:", err);
