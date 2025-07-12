34bb6394b14db9c703e3750da5a52599890d4904
Ariel Nunes da Silva <arielnds274@gmail.com>
2025-07-12 10:32:01 -0300

    fix: updating to run service in production

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,11 +2,11 @@ import app from "./app"
 import { connectDB } from "./config/db_config";
 import "dotenv/config";
 
-const PORT = "4000"
+const PORT = process.env.PORT || 4000;
 
 connectDB().then(() => {
     app.listen(PORT, () => {
-    console.log(`Server running on port http://localhost:${PORT}`)
+    console.log(`Server running on PORT${PORT}`)
    }) 
 }).catch((err) => {
     console.error("Failed to connect to the DB, error:", err);
