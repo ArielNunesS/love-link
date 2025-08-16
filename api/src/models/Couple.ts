a096a04de598df9d05179a9021979d649ec654a4
arielnuness <arielnds274@gmail.com>
2025-08-15 21:18:02 -0300

    fix: couple model

--- api/src/models/Couple.ts
+++ api/src/models/Couple.ts
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
 const CoupleSchema = new mongoose.Schema ({
-    name: {type: String, required: true, maxLength: 15},
-    email: {type: String, required: true, maxLength: 150, unique: true},
-    title: {type: String, required: true, maxLength: 13},
-    message: {type: String, required: true, maxLength: 680},
+    name: {type: String, required: true, maxLength: 20},
+    email: {type: String, required: true, maxLength: 120, unique: true},
+    title: {type: String, required: true, maxLength: 20},
+    message: {type: String, required: true, maxLength: 800},
     startDate: {type: Date, required: true},
     image: {type: String, required: true},
 });
